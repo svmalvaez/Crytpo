995ecf76562fc1eaf3e2f6203c808470c870a77d
Sergio <svmalvaez@gmail.com>
2015-02-25 23:30:33 -0600

    Client sends user data succesfull to server

--- static/js/rsa.js
+++ static/js/rsa.js
@@ -1,24 +1,23 @@
 
 var socket = io('http://localhost:3000/');
 
-// $("#form_user").submit(function(){
-
-// 	var user = {
-// 		"username": this.username,
-// 		"priv_key": this.priv_key,
-// 		"pub_key": this.pub_key,
-// 	}
-// 	console.log(user);
-// 	socket.emit("new_user", user);
-
-// });
-
 $("#send").on("click", function(){
 		var user = {
-		"username": "this.username",
-		"priv_key": "this.priv_key",
-		"pub_key": "this.pub_key",
+			"id": 0,
+		"username": $("#username").val(),
+		"priv_key": $("#priv_key").val(),
+		"pub_key": $("#pub_key").val(),
 	}
+	$("#username, #priv_key, #pub_key").val('');
 	console.log(user);
-	// socket.emit("new_user", user);
+	socket.emit("new_user", user);
 });
+
+socket.on("connected_users", function(data){
+	$("a").detach();
+	$.each(data, function(u, users){
+		$(".list-group").append("<a data-toggle=\"modal\" data-target=\"#conversation\" class=\"list-group-item\">" + u.username + "</a>");
+	});
+});
+
+//"<li class=\"list-group-item\">" + u.username + "</li>"
