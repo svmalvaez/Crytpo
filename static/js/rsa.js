3a1ea401107daf0a27e6892d666d6310bfff0aad
svmalvaez <svmalvaez@gmail.com>
2015-03-04 00:11:13 -0600

    Implementation

--- static/js/rsa.js
+++ static/js/rsa.js
@@ -1,29 +1,43 @@
 var socket = io('http://localhost:3000/');
-var id = 1;
+var passphrase = $("#passphrase").val()
+var userRSAKey = cryptico.generateRSAKey(passphrase,1024);
 
 $("#send").on("click", function(){
-		var user = {
-			"id": id,
+	var userPublicKey = cryptico.publicKeyString(userRSAKey);
+	var user = {
 		"username": $("#username").val(),
-		"priv_key": $("#priv_key").val(),
-		"pub_key": $("#pub_key").val(),
+		"pub_key": userPublicKey,
 	}
-	$("#username, #priv_key, #pub_key").val('');
+	$("#username, #passphrase").val('');
 	console.log(user);
 	socket.emit("new_user", user);
-	id ++;
 });
 
 socket.on("connected_users", function(data){
 	$("a").detach();
 	$.each(data, function(u, users){
-		$(".list-group").append("<a data-toggle=\"modal\" data-target=\"#conversation\" class=\"list-group-item\" id=\""+ u +"\">" + users.username + "</a>");
+		$(".list-group").append("<a data-toggle=\"modal\" data-target=\"#conversation\" data-pub_key = \""+ 
+			users.pub_key +"\" class=\"list-group-item\" id=\""+ u +"\">" + users.username + "</a>");
 	});
 });
 
 
+$('#conversation').on('show.bs.modal', function(e) {
+
+    //get data-pubkey attribute of the clicked element
+    pub_key = $(e.relatedTarget).data('pub_key');
+    $("#message").data("key",pub_key);
+});
+
 $("#send_message").on("click", function(){
-	message = $("#message").val();
+	var RSAKey = cryptico.generateRSAKey("Prueba",1024)
+	message = $("#message").data("key");
+	message_result = cryptico.encrypt(message,pub_key, RSAKey);
+	socket.emit("cypher_message", message_result);
+});
 
-
+socket.on("message", function(data){
+	console.log(userRSAKey);
+	var decrypt= cryptico.decrypt(data.cipher,userRSAKey);
+	console.log(decrypt);
 });
